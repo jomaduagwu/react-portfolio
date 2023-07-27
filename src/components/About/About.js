f1c5fac07d3142a65116981aee0719dd335e10a6
Jochebed Maduagwu <56278106+jomaduagwu@users.noreply.github.com>
2023-07-27 14:46:21 -0500

    Update about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,13 +12,23 @@ function About() {
                         <img className="profile-img" src={aboutPhoto} alt="Jochebed Maduagwu" />
                     </figure>
                 </div>
-                <div className="col-md-6 d-flex alight-items-center">
-                    <p>Hi! My name is Jochebed and I am a full-stack developer in the making, based out of Houston, TX. I'm
-                        a seasoned public health professional with 5+ years of experience focused on maternal and child health, 
-                        infectious diseases, and most recently case investigations for COVID-19. I have always had an interest in
-                        technology and data, which led me to pursue numerous data analytics programs during my career. I have 
-                        recently started my journey as a developer, and I'm excited about this opportunity to further broaden my 
-                        ability to leverage data and technology.
+                <div className="col-md-6 d-block text-center">
+                    <p>Hi! My name is Jochebed and I am a full-stack developer, based out of Houston, TX. I have recently transitioned 
+                        my career from Epidemiology to the dynamic world of web development, bringing a unique perspective to the tech 
+                        industry. During my journey as a developer, I completed a comprehensive 6-month Full Stack Web Development Bootcamp 
+                        at Rice University, honing my skills in both front-end and back-end technologies. I have a keen eye for detail, and 
+                        my ability to gather and analyze data, previously applied in epidemiology, now serves as a powerful asset in debugging 
+                        and improving web applications.
+                    </p>
+                    <p>
+                    My technical skills include proficiency in <strong>HTML, CSS, JavaScript, ReactJS, Node.js, Express.js, MongoDB, and MySQL</strong>. I am adept 
+                    at building interactive and user-friendly web applications that deliver seamless user experiences.
+                    </p>
+                    <p>
+                        As a web developer, I am passionate about creating innovative solutions that positively impact users' lives. I believe 
+                        in the power of technology to drive positive change and am dedicated to making a difference through my work. I am excited 
+                        to contribute my skills and creativity to dynamic web development projects and collaborate with passionate teams that share 
+                        my commitment to excellence.
                     </p>
                 </div>
             </div>
